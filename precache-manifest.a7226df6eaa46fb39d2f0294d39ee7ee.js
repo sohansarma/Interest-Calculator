a683fd25d9b6a2467b1cf3af30c16f87f07bc008
Sohansarma <sohan@troopr.io>
2019-06-08 20:21:45 +0530

    Updates

---
+++ precache-manifest.a7226df6eaa46fb39d2f0294d39ee7ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6c4cfa1c24ac63a682753d112db82c2",
+    "url": "/Interest-Calculator/index.html"
+  },
+  {
+    "revision": "e8fc3ebee0d1eef867f6",
+    "url": "/Interest-Calculator/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "4310d214fc44f3aec8b8",
+    "url": "/Interest-Calculator/static/css/main.face09d7.chunk.css"
+  },
+  {
+    "revision": "e8fc3ebee0d1eef867f6",
+    "url": "/Interest-Calculator/static/js/2.28af92e0.chunk.js"
+  },
+  {
+    "revision": "4310d214fc44f3aec8b8",
+    "url": "/Interest-Calculator/static/js/main.23763b00.chunk.js"
+  },
+  {
+    "revision": "42e36dca05aebdffc369",
+    "url": "/Interest-Calculator/static/js/runtime~main.e609cf70.js"
+  },
+  {
+    "revision": "b104db043e041885a1b8831d840942d2",
+    "url": "/Interest-Calculator/static/media/image2.b104db04.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Interest-Calculator/precache-manifest.a7226df6eaa46fb39d2f0294d39ee7ee.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Interest-Calculator/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
